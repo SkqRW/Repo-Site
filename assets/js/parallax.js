a9b030aa3a47dd4d3f40a68c937dd9f8c8ae16d2
solaristheworstcatever <105955034+solaristheworstcatever@users.noreply.github.com>
2025-03-30 23:18:29 -0600

    lmc

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -3,8 +3,8 @@ var parallax_content_height;
 var parallax_footer_height;
 
 function parallax() {
-    console.log(parallax_content_height);
-    background.style.top = (window.scrollY / (parallax_content_height - window.innerHeight + parallax_footer_height) * (background.offsetHeight - window.innerHeight) * -1) + "px";
+/*     console.log(parallax_content_height);
+ */    background.style.top = (window.scrollY / (parallax_content_height - window.innerHeight + parallax_footer_height) * (background.offsetHeight - window.innerHeight) * -1) + "px";
 }
 
 window.addEventListener("scroll", parallax);
